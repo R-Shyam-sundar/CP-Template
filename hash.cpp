b2eaafb891dafaffd3c395b307aa4aef59768c8e
R-Shyam-sundar <82640234+R-Shyam-sundar@users.noreply.github.com>
2022-12-12 10:42:16 +0530

    Create hash.cpp

---
+++ hash.cpp
@@ -0,0 +1,14 @@
+uint64_t xorshift(const uint64_t& n,int i){
+  return n^(n>>i);
+}
+uint64_t my_hash2(const uint64_t& n){
+  uint64_t p = 0x5555555555555555ull; // pattern of alternating 0 and 1
+  uint64_t c = 17316035218449499591ull;// random uneven integer constant; 
+  return c*xorshift(p*xorshift(n,32),32);
+}
+unsigned int my_hash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
